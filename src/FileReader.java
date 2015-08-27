c60b7145dddab11f505c3755c7b821bf2a667f5e
camodude009 <felix.ma.fischer@gmail.com>
2015-08-27 21:50:43 +0200

    Fixed saving issues - files now loaded/saved in the folder the jar is launched from

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,6 +1,5 @@
+import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,18 +10,18 @@ import java.util.List;
 
 public class FileReader {
 	
-	//reads a file located in the same folder as the jar
+	//reads a file located in the same folder the jar is launched from
 	public static String[] readFile(String fileName){
 		//array for lines
 		List<String> lines = new ArrayList<String>();
 		try {
-			//converting the relative file name to an absolute URL and then to a Path
-			URL url = FileReader.class.getClassLoader().getResource(fileName);
-			Path path = Paths.get(url.toURI());
+			//getting the Path of the file
+			File file = new File(".");
+			Path path = Paths.get(file.getAbsolutePath(), fileName);
 			System.out.println(path);
 			//reading the file
 			lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-		} catch (NullPointerException | IOException | URISyntaxException e) {
+		} catch (NullPointerException | IOException e) {
 			System.out.println("error reading file");
 		}
 		//ignores lines that start with '--'
--- src/FileWriter.java
+++ src/FileWriter.java
@@ -1,8 +1,7 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,15 +9,13 @@ import java.nio.file.Paths;
 
 public class FileWriter {
 
-	//writes to a file located in the same folder as the jar
+	//writes to a file located in the same folder the jar is launched from
 	public static void writeFile(String fileName, String[] lines){
 		try {
-			//converting the relative file name to an absolute URL and then to a Path
-			URL url = FileReader.class.getProtectionDomain().getCodeSource().getLocation();
-			//path of the directory
-			Path dirPath = Paths.get(url.toURI());
-			//path of the file we are trying to create
-			Path path = Paths.get(dirPath.toString(), fileName);
+			//getting the Path of the file
+			File file = new File(".");
+			Path path = Paths.get(file.getAbsolutePath(), fileName);
+			//creating the PrintWriter
 			OutputStream out = Files.newOutputStream(path);
 			PrintWriter writer = new PrintWriter(out);
 			//writing the file
@@ -27,7 +24,7 @@ public class FileWriter {
 				writer.println(s);
 			}
 			writer.close();
-		} catch (URISyntaxException | IOException e) {
+		} catch (IOException e) {
 			System.out.println("error writing file");
 		}
 	}
