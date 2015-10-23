332853c0fdfdf4a3d1ca51337bb6a80b9dca1861
camodude009 <felix.ma.fischer@gmail.com>
2015-10-23 23:43:12 +0200

    Changed size of planets in test system because of new radius scaling Added a second testsystem to test the creation of random planets

--- test.pl
+++ test.pl
@@ -1,5 +1,5 @@
 --this line is a comment and will be ignored
-realtime 0
+realtime 1
 targettime 50000.0
 algo 2
 grav 1.0
@@ -9,7 +9,7 @@ stepsize 0.05
 history -1.0
 historydetail 1
 historygradient 0
-bg 0
-addplanet 0.97000436 -0.24308753 0.466203685 0.43236573 1.0 0.0005 2
-addplanet -0.97000436 0.24308753 0.466203685 0.43236573 1.0 0.0005 3
-addplanet 0.0 0.0 -0.93240737 -0.86473146 1.0 0.0005 4
+bg 1
+addplanet 0.97000436 -0.24308753 0.466203685 0.43236573 1.0 5000.0 2
+addplanet -0.97000436 0.24308753 0.466203685 0.43236573 1.0 5000.0 3
+addplanet 0.0 0.0 -0.93240737 -0.86473146 1.0 5000.0 4
---
+++ test2.pl
@@ -0,0 +1,9 @@
+rand 40
+targettime 1000.0
+stepsize 0.001
+scale 50
+speed 0.25
+history 0
+historydetail 1000
+realtime 0
+pause
